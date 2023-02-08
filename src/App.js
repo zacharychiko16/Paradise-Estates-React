b0915d78233296ee2aad0d2d7192f4c874ad74ef
zacharychiko16 <zacharychiko16@gmail.com>
2023-02-08 19:50:25 -0400

    created sign in, sign up, and forgot password pages and oAuth component

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Route path="/profile" element={<Profile/>}/>
         <Route path="/sign-in" element={<SignIn/>}/>
         <Route path="/sign-up" element={<SignUp/>}/>
-        <Route path="/forgotPassword" element={<ForgotPassword/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
         <Route path="/offers" element={<Offers/>}/>
       </Routes>
     </Router>
