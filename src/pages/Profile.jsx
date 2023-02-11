a7d6d865953fc47e0be5477c8444e55403b48188
zacharychiko16 <zacharychiko16@gmail.com>
2023-02-11 16:22:13 -0400

    Add delete and edit functionality to the my listing section

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -78,6 +78,19 @@ export default function Profile() {
     }
     fetchUserListings();
   }, [auth.currentUser.uid]);
+  async function onDelete(listingID){
+    if (window.confirm("Are you sure you want to delete?")) {
+      await deleteDoc(doc(db, "listings", listingID));
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingID
+      );
+      setListings(updatedListings);
+      toast.success("Successfully deleted the listing");
+    }
+  }
+  function onEdit(listingID){
+    navigate(`/edit-listing/${listingID}`);
+  }
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
